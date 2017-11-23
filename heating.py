ccfbfb9460e862644ed5a20e0ea8f8f1e94cfbb3
Kristof Farkas-Pall <kristof.farkas-pall.14@ucl.ac.uk>
2017-11-23 10:46:21 +0000

    Fix heating

---
+++ heating.py
@@ -0,0 +1,66 @@
+import simtk.openmm.app as app
+import simtk.openmm as mm
+import simtk.unit as u
+
+import numpy as np
+import mdtraj
+import time
+
+# System
+
+prmtop = app.AmberPrmtopFile('complex.top')
+system = prmtop.createSystem(nonbondedMethod=app.PME,
+                             constraints=app.HBonds,
+                             nonbondedCutoff=12*u.angstroms,
+                             switchDistance=10*u.angstroms)
+
+topology = mdtraj.Topology.from_openmm(prmtop.topology)
+
+integrator = mm.LangevinIntegrator(50*u.kelvin, 5/u.picosecond, 0.002*u.picoseconds)
+
+atoms_to_restrain = topology.select('not water and not type H')
+default_k = 4.0*u.kilocalories_per_mole/u.angstroms**2
+
+harmonic_restraint = mm.CustomExternalForce("k*((x-x0)^2+(y-y0)^2+(z-z0)^2)")
+harmonic_restraint.addGlobalParameter('k', default_k)
+harmonic_restraint.addPerParticleParameter("x0")
+harmonic_restraint.addPerParticleParameter("y0")
+harmonic_restraint.addPerParticleParameter("z0")
+
+system.addForce(harmonic_restraint)
+
+# Initialise simulation and positions
+
+simulation = app.Simulation(prmtop.topology, system, integrator)
+
+simulation.reporters.append(DCDReporter('heating.dcd', 10))
+
+simulation.loadState('minimized.xml')
+
+state = simulation.context.getState(getPositions=True)
+positions = state.getPositions()
+
+for restraint in range(harmonic_restraint.getNumParticles()):
+    atomindex, _ = harmonic_restraint.getParticleParameters(restraint)
+    position = positions[atomindex]
+    harmonic_restraint.setParticleParameters(restraint ,atomindex, position.value_in_unit_system(u.md_unit_system))
+
+harmonic_restraint.updateParametersInContext(simulation.context)
+simulation.context.setParameter('k', default_k)
+
+initial_time = time.time()
+
+for temperature in np.linspace(50, 300, 251)*u.kelvin:
+    integrator.setTemperature(temperature)
+    simulation.step(100)
+    print(simulation.context.getState(getEnergy=True).getPotentialEnergy(), temperature/u.kelvin)
+
+simulation.step(500)
+
+final_time = time.time()
+elapsed_time = (final_time - initial_time) * u.seconds
+print('Completed heating in %8.3f s' % (elapsed_time / u.seconds))
+print('Potential energy is %.3f kcal/mol' % (simulation.context.getState(getEnergy=True).getPotentialEnergy() / u.kilocalories_per_mole))
+
+print('Saving state.')
+simulation.saveState('heated.xml')
